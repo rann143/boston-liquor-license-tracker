c3d18b92ee25b00d437cd58445a69c999dca9df1
Daniel <dan.roder17@gmail.com>
2025-05-14 10:33:16 -0400

    make hero responsive

--- client/src/components/pages/home/hero.tsx
+++ client/src/components/pages/home/hero.tsx
@@ -3,11 +3,11 @@ import logo from "@/assets/logo.svg";
 
 const Hero = () => {
   return (
-    <div className="hero w-screen mt-[64px] pt-[200px] pb-[144px] pl-[64px] gap-[24px]">
-      <img src={logo} className="w-5xl" />
-      <h2 className="text-white text-5xl min-w-min whitespace-nowrap">
+    <div className="hero flex flex-col md:items-start md:justify-start w-full sm:w-screen pl-3 pr-3 pt-[100px] pb-[70px] lg:mt-[64px] md:pt-[200px] md:pb-[144px] md:pl-[64px] gap-[24px]">
+      <img src={logo} className="w-2xl lg:w-5xl" />
+      <h2 className="text-white text-2xl md:text-3xl lg:text-5xl w-full ">
         Navigating the Liquor License system in Boston is hard. <br /> We are
-        here to make it just a little bit easier
+        here to make it just a little bit easier.
       </h2>
     </div>
   );
