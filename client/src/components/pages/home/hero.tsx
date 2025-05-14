e758a6de60d5a0e6f56e4d9f5fe050de846af854
Daniel <dan.roder17@gmail.com>
2025-05-13 21:16:39 -0400

    inital commit on hero component

--- client/src/components/pages/home/hero.tsx
+++ client/src/components/pages/home/hero.tsx
@@ -1,11 +1,16 @@
 import "./hero.css";
+import logo from "@/assets/logo.svg";
 
 const Hero = () => {
   return (
-    <div className="hero">
-      <h1>Hero</h1>
+    <div className="hero w-screen mt-[64px] pt-[200px] pb-[144px] pl-[64px] gap-[24px]">
+      <img src={logo} className="w-5xl" />
+      <h2 className="text-white text-5xl min-w-min whitespace-nowrap">
+        Navigating the Liquor License system in Boston is hard. <br /> We are
+        here to make it just a little bit easier
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
